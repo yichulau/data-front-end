1a091192f58d086a8e12d65b4e7174d1da827ca3
Bryan Yong <bryanywy@gmail.com>
2023-01-26 23:23:24 +0800

    Add bar chart and filter selection for number of contracts

--- src/components/charting/BarChart.tsx
+++ src/components/charting/BarChart.tsx
@@ -1,125 +1,135 @@
-import React, { useEffect,useState } from 'react'
-import { ResponsiveBar } from '@nivo/bar'
-import { ResponsivePie } from "@nivo/pie";
+import React, { useEffect, useState, useRef } from 'react'
+import { Select, Form } from "antd";  
+import * as echarts from 'echarts'; 
 
 const BarChart = ({ data }: any) => {
 
+  let dataValues = data; 
+  const chartRef = useRef(null);
+  const { Option } = Select;
+  const [filter, setFilter] = useState(0); // 0 for filter by coin, 1 for filter by exchange
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+    style: { width: "400px" },
+  }; 
+
+  // Filter data by exchange
+  // Sum of total of btc and sum of total of eth across all exchanges
+  const getDataByExchange = () => {
+    let points = []; 
+
+    if (dataValues.length > 0) {
+      points.push(dataValues[0]["count24H"] + dataValues[1]["count24H"]); // Bit.com
+      points.push(dataValues[2]["count24H"] + dataValues[3]["count24H"]); // Binance
+      points.push(dataValues[4]["count24H"] + dataValues[5]["count24H"]); // Deribit 
+      points.push(dataValues[6]["count24H"] + dataValues[7]["count24H"]); // Bybit 
+      points.push(dataValues[8]["count24H"] + dataValues[9]["count24H"]); // OKEX
+    } 
+
+    return {
+      tooltip: {},
+      legend: {
+        data: ["No of Contracts in Past 24 Hours"],
+      },
+      xAxis: {
+        data: ["Bit.com", "Binance", "Deribit", "Bybit", "OKEX"],
+      },
+      yAxis: {
+        name: "Volume",
+      },
+      series: [
+        {
+          name: "No of Contracts in Past 24 Hours",
+          type: "bar",
+          data: points,
+        },
+      ],
+    };  
+  }; 
+
+  // Filter data by coin 
+  // Accumulate all 5 exchange of the btc & eth value,
+  // X-Axis only shows BTC and ETH 
+  const getDataByCoin = () => {
+
+    let sumETH = 0; 
+    let sumBTC = 0; 
+    let points = []; 
+
+    for (let i = 0; i < dataValues.length; i++){
+      if (i % 2 == 0){
+        sumETH += dataValues[i]["count24H"]; 
+      } else {
+        sumBTC += dataValues[i]["count24H"]; 
+      }
+    }; 
+
+    points.push(sumETH); 
+    points.push(sumBTC); 
+
+    return {
+      tooltip: {},
+      legend: {
+        data: ["No of Contracts in Past 24 Hours"],
+      },
+      xAxis: {
+        data: ["ETH", "BTC"],
+      },
+      yAxis: {
+        name: "Volume",
+      },
+      series: [
+        {
+          name: "No of Contracts in Past 24 Hours",
+          type: "bar",
+          data: [sumETH, sumBTC],
+        },
+      ],
+    };    
+  };
+
+  // this will be triggered by the useEffect
+  const getOptions = (value: number) => {
+
+    switch (value) {
+      case 0: 
+        return getDataByCoin(); 
+
+      case 1: 
+        return getDataByExchange(); 
+
+      default: 
+        break; 
+    }
+  }; 
+
+  const handleFilterChange = (value: number) => {
+    setFilter(value); 
+  }; 
+
+  useEffect(() => {
+    let chartInstance = echarts.init(chartRef.current);
+    let options = getOptions(filter); 
+    chartInstance.setOption(options); 
+  }, [data, filter]);
+
   return (
     <>
-            <ResponsiveBar
-                data={data}
-                keys={[
-                    'count24H'
-                ]}
-                indexBy="count24H"
-                margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-                padding={0.3}
-                valueScale={{ type: 'linear' }}
-                indexScale={{ type: 'band', round: true }}
-                colors={{ scheme: 'nivo' }}
-                defs={[
-                    {
-                        id: 'dots',
-                        type: 'patternDots',
-                        background: 'inherit',
-                        color: '#38bcb2',
-                        size: 4,
-                        padding: 1,
-                        stagger: true
-                    },
-                    {
-                        id: 'lines',
-                        type: 'patternLines',
-                        background: 'inherit',
-                        color: '#eed312',
-                        rotation: -45,
-                        lineWidth: 6,
-                        spacing: 10
-                    }
-                ]}
-                fill={[
-                    {
-                        match: {
-                            id: 'fries'
-                        },
-                        id: 'dots'
-                    },
-                    {
-                        match: {
-                            id: 'sandwich'
-                        },
-                        id: 'lines'
-                    }
-                ]}
-                borderColor={{
-                    from: 'color',
-                    modifiers: [
-                        [
-                            'darker',
-                            1.6
-                        ]
-                    ]
-                }}
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'country',
-                    legendPosition: 'middle',
-                    legendOffset: 32
-                }}
-                axisLeft={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'food',
-                    legendPosition: 'middle',
-                    legendOffset: -40
-                }}
-                labelSkipWidth={12}
-                labelSkipHeight={12}
-                labelTextColor={{
-                    from: 'color',
-                    modifiers: [
-                        [
-                            'darker',
-                            1.6
-                        ]
-                    ]
-                }}
-                legends={[
-                    {
-                        dataFrom: 'keys',
-                        anchor: 'bottom-right',
-                        direction: 'column',
-                        justify: false,
-                        translateX: 120,
-                        translateY: 0,
-                        itemsSpacing: 2,
-                        itemWidth: 100,
-                        itemHeight: 20,
-                        itemDirection: 'left-to-right',
-                        itemOpacity: 0.85,
-                        symbolSize: 20,
-                        effects: [
-                            {
-                                on: 'hover',
-                                style: {
-                                    itemOpacity: 1
-                                }
-                            }
-                        ]
-                    }
-                ]}
-                role="application"
-                ariaLabel="Demo"
-            />
-
-
- 
-
+      <div style={{ textAlign: "center" }}>
+        <br />
+        <Form {...layout}>
+          <Form.Item>
+            <Select 
+              defaultValue={filter} 
+              onChange={handleFilterChange}>
+                <Option value={0}>By Currency</Option>
+                <Option value={1}>By Exchange</Option>
+            </Select>
+          </Form.Item>
+        </Form>
+        <div ref={chartRef} style={{ height: "400px", width:"100%" }}></div>
+      </div>
     </>
   )
 }
--- src/components/charting/ChartingCard.tsx
+++ src/components/charting/ChartingCard.tsx
@@ -30,7 +30,7 @@ const ChartingCard = () => {
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Number Of Contract Traded</h5>
             </a>
             {/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p> */}
-            <div className='w-full h-[250px]'>
+            <div className='w-full'>
                 <BarChart data={contractTradedData}/>
             </div>
             

9588cfd37020469ba2a785217ff9530761149627
yichulau <yichu21@gmail.com>
2023-02-27 17:26:02 +0800

    code changes

--- src/pages/analysis/positionBuilder.tsx
+++ src/pages/analysis/positionBuilder.tsx
@@ -11,6 +11,7 @@ import {daysTillExpiry, getCurrentDate} from '../../utils/date';
 import Draggable from "react-draggable";
 import moment from "moment";
 import BlackScholes from "../../utils/blackScholes";
+import { FaPlus, FaChartPie, FaTable } from 'react-icons/fa'
 
 interface PositionProps {
   stockPrice : number,
@@ -482,7 +483,7 @@ const PositionBuilder : React.FC<PositionProps> = () => {
 
   const tabItems = [
     {
-      label: "Search Position",
+      label: "Add Position",
       component: (
         <div className='md:w-full mt-2'>
           <PositionBuilderSearch data={apiData}
@@ -524,7 +525,7 @@ const PositionBuilder : React.FC<PositionProps> = () => {
       ),
     },
     {
-      label: "Expandable",
+      label: "Table",
       component: (
         <div className="bg-white w-full shadow-sm rounded-lg py-2  dark:bg-black">
           <PositionBuilderExpandable dataSet={store} onDelete={handleDelete} handleCheckBoxChanges={handleCheckBoxChanges}/>
@@ -594,7 +595,7 @@ const PositionBuilder : React.FC<PositionProps> = () => {
         <div className="container py-1 mx-auto">
           <div className="flex flex-wrap">
             <div className="px-2 py-2 w-full flex flex-col items-start">
-              <div className='bg-white w-full shadow-sm rounded-t-lg p-4 dark:bg-black h-full'>
+              <div className='bg-white w-full shadow-sm rounded-t-lg p-4 dark:bg-black'>
                 <div className='md:w-full mt-2'>
                   {tabItems[activeTab].component}
                 </div>
@@ -602,23 +603,27 @@ const PositionBuilder : React.FC<PositionProps> = () => {
 
             </div>
 
-            <div className="bg-white flex dark:bg-black w-full justify-center">
-              <div className="border-gray-200 dark:border-gray-700">
-                <div className="flex w-full -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-                  {tabItems.map((item, index) => (
-                    <div className="mx-2" key={index} onClick={() => handleTabClick(index)}>
-                      <div className={`inline-flex p-4 border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group ${activeTab === index ? "bg-blue-200" : ""}`}>
-                        <svg aria-hidden="true" className="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>{item.label}
-                      </div>
-                    </div>
-                  ))}
-                </div>
+            
+              <div className="overflow-hidden border border-gray-100 dark:border-black bg-gray-50 dark:bg-black p-1 w-full">
+                <ul className="flex items-center gap-2 text-sm font-medium">
+                {tabItems.map((item, index) => (
+                    <li className="flex-1"  key={index} onClick={() => handleTabClick(index)}>
+                      <div
+                        className={`flex items-center justify-center gap-2 rounded-lg  px-3 py-2  hover:bg-white hover:text-gray-700 ${activeTab === index ? "bg-white dark:bg-gray-800 text-gra dark:text-white relative shadow" : ""}`}
+                      >
+                        {index === 0 && <FaPlus />}
+                        {index === 1 && <FaChartPie />}
+                        {index === 2 && <FaTable />}
+                        {item.label}</div>
+                    </li>
+                    
+
+                ))}
+                </ul>
               </div>
-            </div>
             
           </div>
         </div>
-
           
         </>
       )}
@@ -628,3 +633,18 @@ const PositionBuilder : React.FC<PositionProps> = () => {
 }
 
 export default PositionBuilder
+
+
+// <div className="bg-white flex dark:bg-black w-full ">
+// <div className="border-gray-200 dark:border-gray-700">
+//   <div className="flex w-full -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+//     {tabItems.map((item, index) => (
+//       <div className="mx-2" key={index} onClick={() => handleTabClick(index)}>
+//         <div className={`inline-flex p-4 border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group ${activeTab === index ? "bg-blue-200" : ""}`}>
+//           <svg aria-hidden="true" className="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>{item.label}
+//         </div>
+//       </div>
+//     ))}
+//   </div>
+// </div>
+// </div>

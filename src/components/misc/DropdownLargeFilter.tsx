b24a0e60e3396391f2481a477c44b46021408829
yichulau <yichu21@gmail.com>
2023-02-27 18:17:52 +0800

    codechanges

--- src/components/misc/DropdownLargeFilter.tsx
+++ src/components/misc/DropdownLargeFilter.tsx
@@ -129,7 +129,7 @@ const DropdownLargeFilter = ({title, resetFlag, options, onChange, exchange, sta
                 className="text-[#58667E] w-full bg-[#EFF2F5] hover:bg-blue-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-800" type="button">{selectedOption}<svg className="w-4 h-4 ml-2 float-right" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
             {isOpen && (
                     <div id="dropdownDelay" 
-                        className={  `block z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-auto md:w-[28rem] dark:bg-gray-700 absolute h-full max-h-[28rem]`}>
+                        className={  `block z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-auto min-w-[24rem] md:w-[26rem] dark:bg-gray-700 absolute h-full max-h-[26rem]`}>
                             {filteredOptions.length > 0 ? (
                                 <>
                                     <div className='flex px-2 py-2'>
@@ -138,7 +138,7 @@ const DropdownLargeFilter = ({title, resetFlag, options, onChange, exchange, sta
                                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                                 <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                                                 </div>
-                                            <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" onChange={(e) => setSearchQuery(e.target.value)} />
+                                            <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" onChange={(e) => setSearchQuery(e.target.value)} />
                                         </div>
                                     </div>
                                     <div className="flex px-2 py-2">
@@ -191,7 +191,7 @@ const DropdownLargeFilter = ({title, resetFlag, options, onChange, exchange, sta
                                             </select>
                                         </div>
                                     </div>        
-                                    <ul className="py-2 text-sm text-gray-700 dark:text-white max-h-64  overflow-y-auto" aria-labelledby="dropdownDelayButton">
+                                    <ul className="py-2 text-sm text-gray-700 dark:text-white max-h-56 md:max-h-64  overflow-y-auto" aria-labelledby="dropdownDelayButton">
                                         {filteredOptions.map((item : any)=>{ 
                                             return ( 
                                                 <li key={item.id} onClick={() => selectOption(item.value)}>
@@ -206,7 +206,7 @@ const DropdownLargeFilter = ({title, resetFlag, options, onChange, exchange, sta
                                 <>
                                 {currency === null || exchanges === null ? (
                                     <>
-                                    <div className="flex items-center text-center justify-center w-auto h-56 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                                    <div className="flex items-center text-center justify-center w-auto h-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
                                         <div className=' px-8'>
                                             <span className="text-gray-700 animate-spin dark:text-white fill-blue-600">Please Ensure Select Currency and</span><br/>
                                             <span className="text-gray-700 animate-spin dark:text-white fill-blue-600">Exchange Before Proceeding</span>
@@ -214,7 +214,7 @@ const DropdownLargeFilter = ({title, resetFlag, options, onChange, exchange, sta
                                     </div>
                                     </>
                                 ) : (
-                                    <div className="flex items-center justify-center w-auto h-56 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                                    <div className="flex items-center justify-center w-auto h-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
                                         <div role="status">
                                             <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
                                             <span className="sr-only">Loading...</span>
